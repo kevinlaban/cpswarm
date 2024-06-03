03ff90e857d485d1b9c05045e52a58b923709ab1
kevinlaban <klaban@delfthyperloop.nl>
2024-06-03 17:22:03 +0200

    First gpt effort of a server, probably kaka

--- path_planning/swarm-functions/area_division/src/main.cpp
+++ path_planning/swarm-functions/area_division/src/main.cpp
@@ -6,13 +6,15 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <mutex>
+#include "your_package_name/PathPlanning.h"
 
-// Global variables to store the current map and a flag to check if the map is received
+std::mutex grid_mutex;
 nav_msgs::OccupancyGrid current_map;
 bool map_received = false;
 
-// Callback function for receiving the map
 void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
+    std::lock_guard<std::mutex> lock(grid_mutex);
     current_map = *msg;
     map_received = true;
     ROS_INFO("Map received: width=%d, height=%d", current_map.info.width, current_map.info.height);
@@ -21,17 +23,11 @@ void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
 bool getRobotPosition(tf::TransformListener &listener, const std::string &robot_frame, geometry_msgs::Point &position) {
     tf::StampedTransform transform_base_to_odom, transform_odom_to_map;
     try {
-        // First check if the robot frame directly transforms to the odom frame
         listener.waitForTransform("odom", robot_frame, ros::Time(0), ros::Duration(3.0));
         listener.lookupTransform("odom", robot_frame, ros::Time(0), transform_base_to_odom);
-
-        // Look up the transform from the odom frame to the map frame
         listener.waitForTransform("map", "odom", ros::Time(0), ros::Duration(3.0));
         listener.lookupTransform("map", "odom", ros::Time(0), transform_odom_to_map);
-
-        // Combine the transformations to get the robot's position in the map frame
         tf::Transform transform_combined = transform_odom_to_map * transform_base_to_odom;
-
         position.x = transform_combined.getOrigin().x();
         position.y = transform_combined.getOrigin().y();
         position.z = transform_combined.getOrigin().z();
@@ -45,9 +41,7 @@ bool getRobotPosition(tf::TransformListener &listener, const std::string &robot_
 std::vector<std::string> getRobotFrames(tf::TransformListener &listener) {
     std::vector<std::string> robot_frames;
     std::vector<std::string> all_frames;
-    
     listener.getFrameStrings(all_frames);
-
     ROS_INFO("Available frames:");
     for (const auto& frame : all_frames) {
         ROS_INFO("%s", frame.c_str());
@@ -55,122 +49,92 @@ std::vector<std::string> getRobotFrames(tf::TransformListener &listener) {
             robot_frames.push_back(frame);
         }
     }
-    
     return robot_frames;
 }
 
-int main(int argc, char **argv) {
-    // Initialize ROS
-    ros::init(argc, argv, "area_division_node");
-    ros::NodeHandle nh;
+bool divideAndPublish(your_package_name::PathPlanning::Request &req, your_package_name::PathPlanning::Response &res) {
+    std::lock_guard<std::mutex> lock(grid_mutex);
 
-    // // Get the number of robots from the parameter server or default to 1
-    // int num_robots;
-    // nh.param("num_robots", num_robots, 1);
-    // Not needed since we have num_robots_detected now
-
-    // Initialize area division object
-    area_division ad;
-
-    // Create a TransformListener
-    tf::TransformListener tf_listener;
-
-    // Retry mechanism to get robot frames
-    std::vector<std::string> robot_frames;
-    for (int i = 0; i < 5; ++i) { // Retry up to 5 times
-        ROS_INFO("Attempting to get robot frames, try %d", i + 1);
-        robot_frames = getRobotFrames(tf_listener);
-        if (!robot_frames.empty()) {
-            break;
-        }
-        ros::Duration(2.0).sleep(); // Sleep for 2 seconds before retrying
+    if (!map_received) {
+        res.success = false;
+        res.message = "No map received yet";
+        return true;
     }
 
+    tf::TransformListener tf_listener;
+    area_division ad;
+    std::vector<std::string> robot_frames = getRobotFrames(tf_listener);
     int num_robots_detected = robot_frames.size();
 
     if (num_robots_detected == 0) {
-        ROS_ERROR("No robot frames found.");
-        return 1;
+        res.success = false;
+        res.message = "No robot frames found.";
+        return true;
     }
 
-    // Create publishers for each robot
+    ad.initialize_map(current_map.info.width, current_map.info.height, current_map.data);
+    std::map<std::string, std::vector<int>> updated_cps_positions;
+    for (const auto& robot_frame : robot_frames) {
+        std::string robot_name = robot_frame.substr(0, robot_frame.find('_'));
+        geometry_msgs::Point position;
+        if (getRobotPosition(tf_listener, robot_frame, position)) {
+            updated_cps_positions[robot_name] = {static_cast<int>(position.x), static_cast<int>(position.y)};
+        } else {
+            ROS_ERROR("Failed to get position for %s", robot_name.c_str());
+        }
+    }
+    ad.initialize_cps(updated_cps_positions);
+    ad.divide();
+
+    ros::NodeHandle nh;
     std::map<std::string, ros::Publisher> robot_pubs;
     std::map<std::string, ros::Publisher> start_pos_pubs;
+
     for (int i = 0; i < num_robots_detected; i++) {
         std::string robot_name = "robot" + std::to_string(i + 1);
-
-    
         std::string grid_topic = "/" + robot_name + "_grid";
         std::string start_pos_topic = "/" + robot_name + "_starting_pos";
-
-        // Create publishers with the new topic names
         ros::Publisher robot_pub = nh.advertise<nav_msgs::OccupancyGrid>(grid_topic, 10);
         ros::Publisher start_pos_pub = nh.advertise<geometry_msgs::Point>(start_pos_topic, 1);
-
         robot_pubs[robot_name] = robot_pub;
         start_pos_pubs[robot_name] = start_pos_pub;
     }
 
-    // Subscriber for the occupancy grid map
-    ros::Subscriber map_sub = nh.subscribe("/map", 10, mapCallback);
-
-    ros::Rate loop_rate(1); // 1 Hz
-    while (ros::ok()) {
-        if (map_received) {
-            ROS_INFO("Processing received map...");
-
-            // Initialize the map in the area_division object
-            ad.initialize_map(current_map.info.width, current_map.info.height, current_map.data);
-
-            // Get robot positions using tf
-            std::map<std::string, std::vector<int>> updated_cps_positions;
-            for (const auto& robot_frame : robot_frames) {
-                std::string robot_name = robot_frame.substr(0, robot_frame.find('_'));
-                geometry_msgs::Point position;
-                if (getRobotPosition(tf_listener, robot_frame, position)) {
-                    updated_cps_positions[robot_name] = {static_cast<int>(position.x), static_cast<int>(position.y)};
-                } else {
-                    ROS_ERROR("Failed to get position for %s", robot_name.c_str());
-                }
+    for (const auto& robot_frame : robot_frames) {
+        std::string robot_name = robot_frame.substr(0, robot_frame.find('_'));
+        int i = 0;
+        nav_msgs::OccupancyGrid robot_grid = ad.get_grid(current_map, robot_name);
+        for (size_t j = 0; j < current_map.data.size(); ++j) {
+            if (current_map.data[j] == -1) {
+                robot_grid.data[j] = 100;
             }
-            ad.initialize_cps(updated_cps_positions);
-
-            // Perform area division
-            ad.divide();
-
-            // Create geometry_msgs::Point messages for starting positions and publish divided maps
-            for (const auto& robot_frame : robot_frames) {
-                std::string robot_name = robot_frame.substr(0, robot_frame.find('_'));
-                int i=0;
-                // Get the divided map for the robot
-                nav_msgs::OccupancyGrid robot_grid = ad.get_grid(current_map, robot_name);
-
-                // Ensure obstacles are marked
-                for (size_t j = 0; j < current_map.data.size(); ++j) {
-                    if (current_map.data[j] == -1) {
-                        robot_grid.data[j] = 100;
-                    }
-                }
-                std::string robot_name_publish = "robot" + std::to_string(i + 1);
-                // Publish the divided map
-                robot_pubs[robot_name_publish].publish(robot_grid);
-
-                // Create and publish starting point
-                std::vector<int> coordinates = updated_cps_positions[robot_name];
-                geometry_msgs::Point start_point;
-                start_point.x = coordinates[0];
-                start_point.y = coordinates[1];
-                start_pos_pubs[robot_name_publish].publish(start_point);
-                i+=1;
-            }
-
-            map_received = false; // Reset the flag
-            ROS_INFO("Map processing complete.");
         }
+        std::string robot_name_publish = "robot" + std::to_string(i + 1);
+        robot_pubs[robot_name_publish].publish(robot_grid);
 
-        ros::spinOnce();
-        loop_rate.sleep();
+        std::vector<int> coordinates = updated_cps_positions[robot_name];
+        geometry_msgs::Point start_point;
+        start_point.x = coordinates[0];
+        start_point.y = coordinates[1];
+        start_pos_pubs[robot_name_publish].publish(start_point);
+        i += 1;
     }
+
+    res.success = true;
+    res.message = "Area divided and maps published successfully";
+    return true;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "area_division_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber map_sub = nh.subscribe("/map", 10, mapCallback);
+    ros::ServiceServer service = nh.advertiseService("divide_area", divideAndPublish);
+
+    ROS_INFO("Ready to divide area and publish maps.");
+
+    ros::spin();
 
     return 0;
 }
--- path_planning/swarm-functions/coverage_path/src/coverage_path.cpp
+++ path_planning/swarm-functions/coverage_path/src/coverage_path.cpp
@@ -1,189 +1,114 @@
 #include "coverage_path.h"
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/Point.h>
 #include <mutex>
+#include "your_package_name/PathPlanning.h"
 
-std::mutex grid_mutex;  // Mutex for thread-safe access to grid variables
- 
-
-//Publishers
-ros::Publisher path_publisher1;
-ros::Publisher path_publisher2;
-ros::Publisher path_publisher3;
-
-// Shared variable to store the latest grid data
+std::mutex grid_mutex;
 nav_msgs::OccupancyGrid latest_robot1_grid;
 nav_msgs::OccupancyGrid latest_robot2_grid;
 nav_msgs::OccupancyGrid latest_robot3_grid;
-
-//Shared variable to store starting positions
 geometry_msgs::Point start_position1;
 geometry_msgs::Point start_position2;
 geometry_msgs::Point start_position3;
 
-
-// Callback functions to handle incoming grid data for each robot
-void robot1GridCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
-    
-
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    if (msg->info.width == 0 || msg->info.height == 0 || msg->info.resolution <= 0.0) {
-        ROS_WARN("Received an invalid grid.");
-        return;
-    }
-    geometry_msgs::Point start_position1;
-    start_position1.x = 0.0;
-    start_position1.y = 0.0;
-
-     // Process the grid data for robot1
-    latest_robot1_grid = *msg; // Store the latest grid
-
-
-    generate_path(start_position1, latest_robot1_grid, path_publisher1);
-}
-
-// Callback for Robot 1 starting position
-void robot1StartPositionCallback(const geometry_msgs::Point::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    start_position1 = *msg;
-    ROS_INFO("Starting position for Robot 1 received - x: %f, y: %f", msg->x, msg->y);
-}
-
-void robot2GridCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
-    ROS_INFO("Grid for Robot 2 received with width %d and height %d", msg->info.width, msg->info.height);
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    if (msg->info.width == 0 || msg->info.height == 0 || msg->info.resolution <= 0.0) {
-        ROS_WARN("Received an invalid grid.");
-        ROS_INFO("RRRRRRRRRRRRR2");
-        return;
-    }
-    // geometry_msgs::Point start_position2;
-    // start_position2.x = 19.0;
-    // start_position2.y = 19.0;
-
-    // Process the grid data for robot1
-    latest_robot2_grid = *msg; // Store the latest grid
-    
-
-
-    generate_path(start_position2, latest_robot2_grid, path_publisher2);
-}
-
-// Callback for Robot 2 starting position
-void robot2StartPositionCallback(const geometry_msgs::Point::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    start_position2 = *msg;
-    ROS_INFO("Starting position for Robot 2 received - x: %f, y: %f", msg->x, msg->y);
-}
-
-void robot3GridCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    if (msg->info.width == 0 || msg->info.height == 0 || msg->info.resolution <= 0.0) {
-        ROS_WARN("Received an invalid grid.");
-        ROS_INFO("RRRRRRRRRRRRR3");
-        return;
-    }
-
-    // Process the grid data for robot1
-    latest_robot3_grid = *msg; // Store the latest grid
-
-
-    generate_path(start_position3, latest_robot3_grid, path_publisher3);
-}
-
-// Callback for Robot 3 starting position
-void robot3StartPositionCallback(const geometry_msgs::Point::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(grid_mutex);
-    start_position3 = *msg;
-    ROS_INFO("Starting position for Robot 3 received - x: %f, y: %f", msg->x, msg->y);
-}
-/**
- * @brief Generate an optimal coverage path for a given area.
- * @param start The starting position of the path.
- * @param roi Pointer to a ROI to cover. Optional, if not given mission area is covered.
- * @return Whether the path has been generated successfully.
- */
-bool generate_path (geometry_msgs::Point start, const nav_msgs::OccupancyGrid& robot_occupancy_map, ros::Publisher& path_publisher)
-{
-    NodeHandle nh;
-
+bool generate_path(geometry_msgs::Point start, const nav_msgs::OccupancyGrid& robot_occupancy_map, ros::Publisher& path_publisher, double resolution) {
+    ros::NodeHandle nh;
     ROS_DEBUG("Starting at (%.2f,%.2f)", start.x, start.y);
 
-    // get area divided per robot
-    ROS_DEBUG("Get map of divided area...");
-
- 
     nav_msgs::OccupancyGrid area = robot_occupancy_map;
-
-    // ROS_INFO("Occupancy Grid Info:");
-    // ROS_INFO("  Width: %d", area.info.width);
-    // ROS_INFO("  Height: %d", area.info.height);
-    // ROS_INFO("  Resolution: %f meters/cell", area.info.resolution);
-    // ROS_INFO("  Origin: (x: %f, y: %f, z: %f)", area.info.origin.position.x, area.info.origin.position.y, area.info.origin.position.z);
-
     ROS_INFO("Generate new coverage path...");
 
-    // construct minimum spanning tree
-    ROS_DEBUG("Construct minimum-spanning-tree...");
+    // Calculate path resolution (distance between path legs)
+    if (resolution == 0) {
+        double fov, overlap;
+        nh.param(this_node::getName() + "/fov", fov, 45.0);
+        nh.param(this_node::getName() + "/overlap", overlap, 0.3);
+        if (area.info.resolution == 0)
+            nh.param(this_node::getName() + "/altitude", resolution, 50.0); // Set default altitude if not provided
+        double width = 2 * resolution * tan(fov * M_PI / 180.0 / 2.0);
+        resolution = width * (1 - overlap);
+    }
+    ROS_DEBUG("Map resolution: %.2f", resolution);
+
     spanning_tree tree;
     tree.initialize_graph(area, vertical);
     tree.construct();
 
-    // visualize path
     if (visualize)
         mst_publisher.publish(tree.get_tree());
 
-    // generate path
-    ROS_DEBUG("Generate coverage path...");
     path.initialize_map(area, 0, vertical);
-    // Check here
     path.initialize_tree(tree.get_mst_edges());
     if (!path.generate_path(start)) {
-        ROS_WARN("Failed to generate path for the robottarting at (%.2f,%.2f)", start.x, start.y);
+        ROS_WARN("Failed to generate path for the robot starting at (%.2f,%.2f)", start.x, start.y);
         return false;
-        }
+    }
     if (turning_points)
         path.reduce();
 
-    // visualize path
-    // if (visualize)
     ROS_INFO("Visualize new coverage path...");
-
     path_publisher.publish(path.get_path());
-
 
     return true;
 }
 
-/**
- * @brief A ROS node that computes the optimal paths for area coverage with a swarm of CPSs.
- * @param argc Number of command line arguments.
- * @param argv Array of command line arguments.
- * @return Success.
- */
-int main (int argc, char **argv)
-{
-    // init ros node
-    init(argc, argv, "coverage_path");
-    NodeHandle nh;
+bool planPaths(your_package_name::PathPlanning::Request &req, your_package_name::PathPlanning::Response &res) {
+    std::lock_guard<std::mutex> lock(grid_mutex);
 
-    path_publisher1 = nh.advertise<nav_msgs::Path>("coverage_path/path1", 1, true);
-    // path_publisher2 = nh.advertise<nav_msgs::Path>("coverage_path/path2", 1, true);
-    // path_publisher3 = nh.advertise<nav_msgs::Path>("coverage_path/path3", 1, true);
-  
+    ros::NodeHandle nh;
+    ros::Publisher path_publisher1 = nh.advertise<nav_msgs::Path>("coverage_path/path1", 1, true);
+    ros::Publisher path_publisher2 = nh.advertise<nav_msgs::Path>("coverage_path/path2", 1, true);
+    ros::Publisher path_publisher3 = nh.advertise<nav_msgs::Path>("coverage_path/path3", 1, true);
 
-    // Subscribers for the divided maps
+    if (latest_robot1_grid.info.width > 0 && latest_robot1_grid.info.height > 0) {
+        if (!generate_path(start_position1, latest_robot1_grid, path_publisher1, req.resolution)) {
+            res.success = false;
+            res.message = "Failed to generate path for robot 1";
+            return true;
+        }
+    }
+
+    if (latest_robot2_grid.info.width > 0 && latest_robot2_grid.info.height > 0) {
+        if (!generate_path(start_position2, latest_robot2_grid, path_publisher2, req.resolution)) {
+            res.success = false;
+            res.message = "Failed to generate path for robot 2";
+            return true;
+        }
+    }
+
+    if (latest_robot3_grid.info.width > 0 && latest_robot3_grid.info.height > 0) {
+        if (!generate_path(start_position3, latest_robot3_grid, path_publisher3, req.resolution)) {
+            res.success = false;
+            res.message = "Failed to generate path for robot 3";
+            return true;
+        }
+    }
+
+    res.success = true;
+    res.message = "Paths generated successfully";
+    return true;
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "coverage_path_node");
+    ros::NodeHandle nh;
+
     ros::Subscriber robot1_sub = nh.subscribe("/robot1_grid", 10, robot1GridCallback);
-    // ros::Subscriber robot2_sub = nh.subscribe("robot2_grid", 10, robot2GridCallback);
-    // ros::Subscriber robot3_sub = nh.subscribe("robot3_grid", 10, robot3GridCallback);
+    ros::Subscriber robot2_sub = nh.subscribe("/robot2_grid", 10, robot2GridCallback);
+    ros::Subscriber robot3_sub = nh.subscribe("/robot3_grid", 10, robot3GridCallback);
 
-    // Subscriber for the starting position of the Robots
-    ros::Subscriber startPosSub_R1 = nh.subscribe("robot1_starting_pos", 10, robot1StartPositionCallback);
-    // ros::Subscriber startPosSub_R2 = nh.subscribe("R2_starting_pos", 10, robot2StartPositionCallback);
-    // ros::Subscriber startPosSub_R3 = nh.subscribe("R3_starting_pos", 10, robot3StartPositionCallback);
+    ros::Subscriber startPosSub_R1 = nh.subscribe("/robot1_starting_pos", 10, robot1StartPositionCallback);
+    ros::Subscriber startPosSub_R2 = nh.subscribe("/robot2_starting_pos", 10, robot2StartPositionCallback);
+    ros::Subscriber startPosSub_R3 = nh.subscribe("/robot3_starting_pos", 10, robot3StartPositionCallback);
 
+    ros::ServiceServer service = nh.advertiseService("plan_paths", planPaths);
 
-    ROS_INFO("Path generation action server available");
+    ROS_INFO("Ready to plan paths.");
 
-    spin();
+    ros::spin();
 
     return 0;
 }
